bb94fe4d3d688d73686acc0b8c7f029ddacd572c
AxonSkie <axonmatalubos@gmail.com>
2024-10-09 20:30:56 +0800

    Committed

--- app/HomeCart.tsx
+++ app/HomeCart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/redux/store";
 import {
